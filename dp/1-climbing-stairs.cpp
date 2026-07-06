caa420b2270aa5fdf68ecac76009752b2807b93a
Saarthek-Raj <f20220137@hyderabad.bits-pilani.ac.in>
2026-07-06 22:09:43 +0530

    Memoized Solution

--- dp/1-climbing-stairs.cpp
+++ dp/1-climbing-stairs.cpp
@@ -10,8 +10,9 @@ using namespace std;
 class Solution {
 public:  
     //Recursion
-    int climbStairs(int n) {
+    /*int climbStairs(int n) {
         if(n<=1){
+            // This shows that we reached the end showing that the current recursion stack shows a possible sollution
             return 1;
         }
         else{
@@ -19,7 +20,26 @@ public:
         }
 
     }
+    //Time Complexity -> Exponential
+    //Space Complexity -> O(n) (Recursion Stack)    
+    */
    //Memoization
+    int climbStairs(int n) {
+        vector <int> dp(n+1, -1);
+        int ans = memoizedClimbStairs(n, dp);
+        return ans;
+    }
+    int memoizedClimbStairs(int i, vector<int> & dp){
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        if(i<=1){
+            return (dp[i] = 1);
+        }
+        return (dp[i] = memoizedClimbStairs(i-1, dp) + memoizedClimbStairs(i-2, dp));
+    }
+    //Time Complexity -> O(n)
+    //Space Complexity -> O(n) 1D DP array and Recursion Stack
 };
 
 int main(){
