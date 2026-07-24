42e5c99a85ac7ca4942446f1e3d0a6e9b4610dab
Saarthek-Raj <f20220137@hyderabad.bits-pilani.ac.in>
2026-07-24 21:33:19 +0530

    Memoization

---
+++ dp/12-subset-sum.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+  private:
+    int recSubsetSum(vector<int>& arr, vector<vector<int>> & dp, int sum, int st, int end){
+        if(dp[sum][st]!=-1){
+            return dp[sum][st];
+        }
+        if(sum==0){
+            return dp[sum][st]=true;
+        }
+        if(sum!=arr[st]&&st==end){
+            return dp[sum][st] = false;
+        }
+        dp[sum][st] = false;
+        if(sum-arr[st]>=0){
+            dp[sum][st] = recSubsetSum(arr, dp, sum-arr[st], st+1, end);
+        }
+        return dp[sum][st] = dp[sum][st] || recSubsetSum(arr, dp, sum, st+1, end);
+    }
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(sum+1, vector<int>(n+1, -1));
+        return recSubsetSum(arr, dp, sum, 0, n-1);
+    }
+};
+
+int main(){
+    int n, sum;
+    cin >> n >> sum;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    Solution sol;
+    bool ans = sol.isSubsetSum(arr, sum);
+}
