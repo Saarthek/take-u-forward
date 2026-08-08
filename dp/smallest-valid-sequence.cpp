4b465ee7f5816b408b16331aa0051f9cdc3f2f8a
Saarthek <saarthekraj@gmail.com>
2026-08-08 18:08:07 +0530

    Memoized version

--- dp/smallest-valid-sequence.cpp
+++ dp/smallest-valid-sequence.cpp
@@ -13,6 +13,8 @@
 using namespace std;
 
 class Solution {
+/*
+//Recursion
 private:
     bool recValidSeq(string &word1, string &word2, int i, int j, int n1, int n2,
                      vector<int>& ans, bool onech) {
@@ -51,6 +53,49 @@ public:
     }
     // Time Complexity: O(2^n1)
     // Space Complexity: O(n1)
+    */
+//Memoization
+private:
+    int recValidSeq(string &word1, string &word2, vector<vector<vector<int>>> &dp, int i, int j, int n1, int n2, vector<int>& ans, bool onech){
+        int &m = dp[i][j][onech];
+        if(m!=-1){
+            return m;
+        }
+        int chkp = ans.size();
+        if(j>=n2){
+            return m=true;
+        }
+        if(i>=n1){
+            return m=false;
+        }
+        if(word1[i]==word2[j]){
+            ans.push_back(i);
+            return m=recValidSeq(word1, word2, dp, i+1, j+1, n1, n2, ans, onech);
+        }
+        else if(!onech){
+            ans.push_back(i);
+            if(recValidSeq(word1, word2, dp, i+1, j+1, n1, n2, ans, true)){
+                return m=true;
+            }
+            ans.resize(chkp);
+        }
+        return m=recValidSeq(word1, word2, dp, i+1, j, n1, n2, ans, onech);
+    }
+public:
+    vector<int> validSequence(string word1, string word2) {
+        // w1[i]==w2[j] -> include
+        // w1[i]!=w2[j] -> exclude, or pick (pick if not one dissimilar was not picked)
+        vector<int> ans;
+        int n1 = word1.size();
+        int n2 = word2.size();
+        vector<vector<vector<int>>> dp(n1+1, vector<vector<int>>(n2+1, vector<int>(2, -1)));
+        if(recValidSeq(word1, word2, dp, 0, 0, n1, n2, ans, false)){
+            return ans;
+        }
+        return vector<int>(0);
+    }
+    // Time Complexity: O(n1*n2)
+    // Space Complexity: O(n1*n2) (DP array + stack space)
 };
 
 int main() {
